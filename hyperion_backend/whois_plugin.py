e0e80567f7ce522884215ea6e5011729256f9ced
rep1711 <rep1711@Repsol.com>
2021-11-22 00:13:39 +0200

    Initial commit

---
+++ hyperion_backend/whois_plugin.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request
+
+whois_bp = Blueprint('whois-bp', __name__, url_prefix='/')
+
+
+@whois_bp.route('/whois', methods=('GET', 'POST'))
+def get_whois_data():
+    from .celery_worker import get_whois_data_async
+
+    domain = request.args.get("domain")
+    res = get_whois_data_async.apply_async(args=[domain])
+    return res.get()
